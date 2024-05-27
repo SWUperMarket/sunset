bb60344d635890b65592c23aaffcf91188bdc5c5
woohyeonCho <109808283+woohyeonCho@users.noreply.github.com>
2024-05-28 05:10:31 +0900

    Update settings.py
    settings.py에 ALLOWED_HOSTS에 주소를 추가합니다.

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]
 
 # Application definition
 
